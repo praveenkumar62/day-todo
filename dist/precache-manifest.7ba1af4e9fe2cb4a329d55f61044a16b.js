f23c4c936c7c5a80f77a75e53096c3b3c1aaef87
PraveenC <praveenkumar7501@gmail.com>
2020-02-21 20:43:29 +0530

    21-feb-20

---
+++ dist/precache-manifest.7ba1af4e9fe2cb4a329d55f61044a16b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2235a3b75cb117d0f8e2417553bd4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "035023bcd31b7a35045e",
+    "url": "/static/css/main.1f10553c.chunk.css"
+  },
+  {
+    "revision": "8d22a5c746a80211f86c",
+    "url": "/static/js/2.5610cd6f.chunk.js"
+  },
+  {
+    "revision": "035023bcd31b7a35045e",
+    "url": "/static/js/main.1f4799b8.chunk.js"
+  },
+  {
+    "revision": "aedf08bfd231bc1f44d7",
+    "url": "/static/js/runtime~main.7677ad42.js"
+  }
+]);
